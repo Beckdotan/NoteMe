470116bb1c474eae82055334e209126b6ed12134
Beckdotan <dotan.beck@gmail.com>
2022-01-18 10:40:48 +0200

    spash screen is working

--- app/src/main/java/com/example/noteme/MainActivity.java
+++ app/src/main/java/com/example/noteme/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.noteme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    //handler for splashscreen
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //cosmetics for splash screen
+        getSupportActionBar().hide();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
