f75a8ba6015177ea73dd892de7f98c1ee7cc7272
Beckdotan <dotan.beck@gmail.com>
2022-01-18 16:36:16 +0200

    working map + button to get to your location but getDiviceLocation Doesnt work

--- app/src/main/java/com/example/noteme/MapActivity.java
+++ app/src/main/java/com/example/noteme/MapActivity.java
@@ -1,15 +1,17 @@
 package com.example.noteme;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +19,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.noteme.databinding.ActivityMapBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,6 +28,12 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private ActivityMapBinding binding;
 
     private static final int LOCATION_PERMISSION_CODE = 101;
+    private FusedLocationProviderClient mfusedLocationProviderClient;
+    private static final int DEFAULT_ZOOM = 15;
+    private final LatLng mDeaultlocation = new LatLng(-34, 151);
+    private Location mLastKnownLocation;
+    private boolean mLocationPermissionGranted = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +47,11 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        if (isLocationPremissionGranted()){
+        if (mLocationPermissionGranted) {
             Log.i("mapActivity", "onCreate: permission granted");
-        }else{
-            requestLocationPremission();
+        } else {
+            requestLocationPermission();
         }
-
     }
 
     /**
@@ -58,29 +67,93 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(mDeaultlocation).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDeaultlocation, DEFAULT_ZOOM));
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
             mMap.setMyLocationEnabled(true);
         }
 
+        updateLocationUI();
+        //getDeviceLocation();
+
     }
 
-    //check if location is premitted
-    private boolean isLocationPremissionGranted(){
+    //check if location is permitted
+    private void isLocationPermissionGranted() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }else{
-            return false;
+            mLocationPermissionGranted = true;
+            return;
+        } else {
+            return;
         }
     }
+
+
     //asking for location permission
-    private  void requestLocationPremission(){
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
     }
+
+
+    //getting last known location and moving the map to it.
+    private void getDeviceLocation() {
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Task locationResult = mfusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            //set the cameras positions to the current location of device.
+
+                            mLastKnownLocation = (Location) task.getResult();
+                            Log.i("My Location", mLastKnownLocation.toString());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+
+                        } else {
+                            Log.d("nullLocation", "current location is null, Using Defult");
+                            Log.e("nullLocation", "Exception %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDeaultlocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            }else{
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                requestLocationPermission();
+            }
+
+        } catch (SecurityException e){
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+
 }
